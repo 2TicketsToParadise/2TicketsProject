ac5dd1cc94e3a4e4f3f0b17e2289dbe31a22262d
Nikhil Sulegaon <nikhilsulegaon@gmail.com>
2018-12-05 22:33:40 -0700

    Update CallTest.js

--- screens/CallTest.js
+++ screens/CallTest.js
@@ -6,6 +6,8 @@ import { Text, View, } from 'react-native';
 export default class CallTest extends Component {
     constructor(props) {
         super(props);
+        this.state = { isLoading: true}
+
         this.state = {
           firstName: '',
           lastName: '',
@@ -16,16 +18,50 @@ export default class CallTest extends Component {
         };
     }
 
+    
+    componentDidMount() {
+        return dbCall('select * from users where age = 29;', function(responseData) {
+            // Note: This function will be executed inside of the dbCall function when the API responds with data
+            
+            // TODO: Add Safety checks -> length of responseData
+            var state = {isLoading: false,
+                         firstName: responseData[0].firstName,
+                         lastName: responseData[0].lastName,
+                         age: responseData[0].age,
+                         weight: responseData[0].weight,
+                         height: responseData[0].height
+                        };
+            this.setState(state, function(){});});
+        })
+        
+        
+        
+        return fetch('https://facebook.github.io/react-native/movies.json')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({isLoading: false,
+                               dataSource: responseJson.movies}, function(){});})
+            .catch((error) => {console.error(error);});
+    }
+    
+    
 
     render() {
+        
+        if(this.state.isLoading){
+            return(
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
         return (
             <View>
-                <Text> Hello, {this.props.firstName} {this.props.lastName} </Text>
-                <Text> Age: {this.props.age} </Text>
-                <Text> Weight: { this.props.weight} </Text>
-                <Text> Height: { this.props.height} </Text>
+                <Text> Hello, {this.state.firstName} {this.state.lastName} </Text>
+                <Text> Age: {this.state.age} </Text>
+                <Text> Weight: { this.state.weight} </Text>
+                <Text> Height: { this.state.height} </Text>
             </View>
         )
-
     }
 }
